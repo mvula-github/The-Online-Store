f79bfc2f8b9a7ca14e21031fe123e7c12733247a
mvula <mvulakolweni08@gmail.com>
2025-05-14 23:24:06 +0200

    combined orders list and delveries for easier analytics

---
+++ frontend/src/components/CombinedView.jsx
@@ -0,0 +1,11 @@
+import OrdersList from "./OrdersList";
+import DeliveriesList from "./DeliveriesList";
+
+export default function CombinedView() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <OrdersList />
+      <DeliveriesList />
+    </div>
+  );
+}
